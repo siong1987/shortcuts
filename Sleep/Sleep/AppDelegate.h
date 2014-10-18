82337af9bdbc0a121e64b98bc4b30637fc4f96ea
Siong <siong1987@gmail.com>
2014-10-18 03:44:58 -0700

    add Sleep app.

---
+++ Sleep/Sleep/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sleep
+//
+//  Created by Teng Siong Ong on 10/18/14.
+//  Copyright (c) 2014 Teng Siong Ong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
