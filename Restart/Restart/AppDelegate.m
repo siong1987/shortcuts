48f5be07274f6f3914ec058f84bfe2991534870b
Siong <siong1987@gmail.com>
2014-10-18 03:51:59 -0700

    add Restart app.

---
+++ Restart/Restart/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Restart
+//
+//  Created by Teng Siong Ong on 10/18/14.
+//  Copyright (c) 2014 Teng Siong Ong. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "MDRestartShutdownLogout.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  MDSendAppleEventToSystemProcess(kAERestart);
+
+  // Quit the app
+  [[NSApplication sharedApplication] terminate:nil];
+}
+
+@end
